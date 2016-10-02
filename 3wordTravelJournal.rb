7468b82cffd63c5ab6fbb454f34353cf9f427f30
jbird9 <jordantobybird@gmail.com>
2016-10-02 00:56:09 -0400

    now it only collects 120 unique location so that it only takes 2 minutes to run

--- 3wordTravelJournal.rb
+++ 3wordTravelJournal.rb
@@ -10,17 +10,7 @@ w3wkey = "BHBTANZT"
 jsonfile = File.read(ARGV[0])
 data_hash = JSON.parse(jsonfile)
 
-#lat = ARGV[0]
-#long = ARGV[1]
-
-url = "www.google.com"
-#puts lat
-#puts long
-#url = "https://api.what3words.com/v2/reverse?coords=#{lat}%2C#{long}&key=#{w3wkey}&lang=en&format=json&display=full"
-
-url_str = "\'#{url}\'"
-#puts url_str
-#puts `curl --request GET --url #{url_str}`
+collect_locations = 120
 
 lats = []
 longs = []
@@ -38,14 +28,16 @@ puts '<lines title="what3wordstraveljournalhaiku" provider="google_location" sof
 
 words = []
 syllable_len = []
-(0..lats_longs.length-1).each do |j|
-	url = "https://api.what3words.com/v2/reverse?coords=#{lats_longs.keys[j].to_s.insert(-8,'.')}%2C#{lats_longs.values[j].to_s.insert(-8,'.')}&key=#{w3wkey}&lang=en&format=json&display=full"
+collect_locations.times do
+	rand_loc = rand(0..lats_longs.length-1)
+	url = "https://api.what3words.com/v2/reverse?coords=#{lats_longs.keys[rand_loc].to_s.insert(-8,'.')}%2C#{lats_longs.values[rand_loc].to_s.insert(-8,'.')}&key=#{w3wkey}&lang=en&format=json&display=full"
 	url_str = "\'#{url}\'"
 	jsonout = `curl --request GET --url #{url_str}`
 	data_hash1 = JSON.parse(jsonout)
 	words << data_hash1["words"].split('.').join(' ')
 	syllable_len << Lingua::EN::Syllable.syllables(data_hash1["words"].split('.').join(' '))
 end
+
 word_syl = words.zip(syllable_len).to_h
 
 (0..word_syl.length-1).each do |k|
